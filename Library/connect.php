3cf042e74c61e669d4767f27c9867112f351c2bd
Huy <huyb1505883@student.ctu.edu.vn>
2018-01-23 15:48:06 +0700

    Pull connect.php

--- Library/connect.php
+++ Library/connect.php
@@ -1,3 +1,5 @@
-<?php 
-mysqli_connect(''"localhost","root","Watch"'');
+<?php
+$conn = mysqli_connect('localhost','root','','watch') or die("Không thể kết nối với CSDL:".mysqli_error($conn));
+//mysqli_character_set_name($conn,'utf8');
+mysqli_set_charset($conn,'utf8');
 ?>
